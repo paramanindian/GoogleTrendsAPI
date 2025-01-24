3c984c640f36b119a11e3f614f06d5bee42aad51
paramanindian <paramscientist@gmail.com>
2025-01-24 20:16:38 +0530

    updated

--- app.py
+++ app.py
@@ -5,6 +5,25 @@ import pandas as pd
 app = Flask(__name__)
 pytrends = TrendReq(hl='en-US', tz=360)
 
+
+@app.route('/google-trends', methods=['POST'])
+def run_get_trends():
+    try:
+        data = request.get_json()  # This will read the JSON body of the request
+        print(f"Received data: {data}")  # Log the data to verify it's coming through
+        # Start the long-running task in a separate thread
+        pytrends = TrendReq(hl='en-US', tz=360)
+        trending_searches = pytrends.trending_searches(pn='united_states')
+        dl1 = trending_searches[0].to_list()
+
+        # Immediately respond with a message
+        return jsonify({
+            'message': dl1
+        }), 200
+    except subprocess.CalledProcessError as e:
+        return f"Error executing Python script: {str(e)}", 500
+
+
 @app.route('/trends', methods=['GET'])
 def get_trends():
     try:
